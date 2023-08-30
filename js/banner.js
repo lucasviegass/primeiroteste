3021547d04e97ffcaa005a4865e7eff1f9dd1c06
LUCAS VIEGAS DA COSTA <ART0060943@aluno.firjansenaisesi.com.br>
2023-08-30 11:14:58 -0300

    setInterval

--- js/banner.js
+++ js/banner.js
@@ -1,9 +1,13 @@
-class banner{
-CriarBanner(IdAlvo,Texto){
-let Elemento = document.getElementById(IdAlvo)
-Elemento.innerHTML +="<article>"+Texto+"</article>"
+class banner {
+    CriarBanner(IdAlvo, ListaBanner) {
+        let Elemento = document.getElementById(IdAlvo)
+        Elemento.innerHTML += "<article></article>"
 
-}
+    }
+    ModificaBanner(IdAlvo = "", ListaBanner = [""], Index = 0) {
+        let banner = document.getElementById(IdAlvo).getElementsByTagName("article")[0]
+        banner.innerHTML= ListaBanner[Index]
+    }
 }
 
 export default banner
--- js/main.js
+++ js/main.js
@@ -7,4 +7,16 @@ nav.CriarNovoMenu("barra_01",Menus)
 nav.CriarBarraNavUsuario("barra_01","Home")
 
 let B = new banner ()
-B.CriarBanner("banner_01","<h1>Compra Rápida</h1></br><h3>Compre roupas mais rápido</h3>")
+let BannerInfo = ["<h1>Compra Rápida</h1></br><h3>Compre roupas mais rápido</h3>","<h1>Roupas da Moda</h1></br><h3>Moda</h3>","<h1>Oferta relampago</h1>"]
+B.CriarBanner("banner_01")
+B.ModificaBanner("banner_01",BannerInfo,2)
+
+
+let BannerIndex = 0 
+setInterval(()=>{
+if(BannerIndex >=2){
+    BannerIndex = 0
+}
+B.ModificaBanner("banner_01",BannerInfo,BannerIndex)
+BannerIndex+=1
+},2000)
